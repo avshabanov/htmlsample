5dbe2a990565564af4084f27679d38a0ff36dfe5
Alexander Shabanov <avshabanov@gmail.com>
2015-10-27 22:27:40 -0700

    Merging with todo demo

--- babel/demo-app/js/components/SimpleApp.react.js
+++ babel/demo-app/js/components/SimpleApp.react.js
@@ -4,14 +4,27 @@
 import {Container} from 'flux/utils';
 import React, {Component} from 'react';
 
+import TodoStore from '../flux-infra/todo/TodoStore';
+
+import MainSection from './MainSection.react';
+
+type State = {
+  todos: Immutable.Map<string, Todo>,
+  areAllComplete: boolean,
+  greeting: string
+};
+
+
 class SimpleApp extends Component<{}, {}, State> {
   static getStores(): Array<Store> {
-    return [];
+    return [TodoStore];
   }
 
   static calculateState(prevState: ?State): State {
     return {
-      greeting: 'Hello'
+      greeting: 'Hello',
+      todos: TodoStore.getState(),
+      areAllComplete: TodoStore.areAllComplete()
     };
   }
 
@@ -19,12 +32,12 @@ class SimpleApp extends Component<{}, {}, State> {
     return (
       <div>
         <p>Hello from demo <strong>react</strong> webapp!!</p>
+        <MainSection todos={this.state.todos} areAllComplete={this.state.areAllComplete} />
+        <hr/>
       </div>
     );
   }
 }
 
 const SimpleAppContainer = Container.create(SimpleApp);
-
-export default SimpleApp;
-
+export default SimpleAppContainer;
